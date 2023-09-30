9f6d69cce2a6b4de30d349abdb537fb4f9dc225a
Ritz Dev <ritzocite@gmail.com>
2023-09-30 23:49:31 +1300

    connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
-import  express from "express";
+import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log("CONNECTED to MongoDB");
+    }).catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log("Server listening to PORT:3000");
 });
