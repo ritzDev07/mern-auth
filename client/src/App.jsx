cc3a27152e0e002f01d7feb4c5e415243ba2829d
Ritz Dev <ritzocite@gmail.com>
2023-10-04 19:05:21 +1300

    Update header component and make profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import { BrowserRouter , Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
     return (
         <BrowserRouter>
-        {/* HEADER */}
-        <Header />
+            {/* HEADER */}
+            <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/sign-in' element={<Signin />} />
                 <Route path='/sign-up' element={<Signup />} />
-                <Route path='/profile' element={<Profile />} />
+                <Route element={<PrivateRoute />} >
+                    <Route path='/profile' element={<Profile />} />
+                </Route>
             </Routes>
-        </BrowserRouter>
+        </BrowserRouter >
     )
 }
 
