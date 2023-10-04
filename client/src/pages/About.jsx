1f6a0df4797ec1b83082850415619be8cfa6f7f7
Ritz Dev <ritzocite@gmail.com>
2023-10-05 01:21:37 +1300

    Complete Home and About pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>About</div>
+        <div className='px-4 py-12 max-w-2xl mx-auto'>
+            <h1 className=' text-3xl font-bold mb-4'>About</h1>
+            <p className='mb-4 text-slate-700'> The front end of this app is all about React – we've got client-side routing with React Router. On the backend, it's Node.js and Express holding things together, and our trusty data buddy is MongoDB. 🌟</p>
+            <p className='mb-4 text-slate-700'>Now, about security – we've got your back with JSON Web Tokens (JWTs) for authentication. So you can trust that your data stays safe and sound.</p>
+            <p className='mb-4 text-slate-700'>But here's the best part – this app is like your coding playground! It's a template you can use to cook up your very own full-stack web apps with authentication using the MERN stack. So let your ideas run wild and make some digital magic happen! 🪄💻✨</p>
+        </div>
     )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='px-4 py-12 max-w-2xl mx-auto'>
+            <h1 className=' text-3xl font-bold mb-4'>Welcome to my full-stack web app, built with the MERN stack! 🚀</h1>
+            <p className='mb-4 text-slate-700'> Think of it as your all-in-one toolbox for web development. We've got MongoDB for data storage, Express for smooth server-side action, React for the front-end flair, and Node.js tying it all together. 💪</p>
+            <p className='mb-4 text-slate-700'> And when it comes to security, we've got your back. Our authentication system lets users register, log in, and log out – all while keeping those protected routes locked down tight. 🛡️</p>
+        </div>
     )
 }
 
